b5e1f5ac41fbfc0e4336e7dd9f4ca506b8af92bb
新水鍵二郎 <shinsuikenjiro@shinsuikagijirounoMacBook-Pro.local>
2020-01-26 16:01:03 +0900

    resolved errors

--- bookers2-debug/app/controllers/application_controller.rb
+++ bookers2-debug/app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!, except: [:top]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	#デバイス機能実行前にconfigure_permitted_parametersの実行をする。
 	protect_from_forgery with: :exception
-
-  protected
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   #sign_out後のredirect先変更する。rootパスへ。rootパスはhome topを設定済み。
@@ -14,8 +11,11 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     #sign_upの際にnameのデータ操作を許。追加したカラム。
   end
 end
